d80e8f660d0c1872cda33142866427f50a2979a7
Isa <isasatake7@gmail.com>
2022-11-04 13:31:52 -0400

    tutorial12 + my homepage

---
+++ lectures/lecture19/main.js
@@ -0,0 +1,31 @@
+// ignore this function for now. We'll go over it
+// on Wednesday: 
+async function fetchCourses() {
+    const url = `https://meteor.unca.edu/registrar/class-schedules/api/v1/courses/2022/fall/`;
+    data = await fetch(url).then(response => response.json());
+    displayResults(data);
+} 
+/* Your Tasks:
+    1. Output the title of the first course to the console.
+    2. Output the name of the course's instructor to the console.
+    3. Output the title of the first course to the #results section.
+    4. Output the title and the instructor of the first course to the #results section.
+    5. Output ALL of the course titles to the console.
+    6. Output ALL of the course titles to the #results section.
+    7. Output ALL of the diversity intensive courses to the results section.
+    8. Questions to ponder:
+        * How could you make this interface 
+          more useful and engaging?
+        * How could you allow your user to...
+            * Select which department they want to view?
+            * Select which term they want to view?
+            * Only view classes that meet on Tuesdays and Thursdays? 
+*/
+function displayResults(data) {
+    console.log(data); //display all data to console
+
+    console.log(data[0].Title);  //display first item's title to console
+    document.querySelector("#results").innerHTML = data[0].Title; //display first item's title to HTML 
+
+    console.log(data[0].Instructors[0].Name);
+}
