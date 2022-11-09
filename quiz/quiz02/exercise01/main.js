abf873a77c1c3236e11aab32ac864473f20bff27
Isa <isasatake7@gmail.com>
2022-11-09 15:23:37 -0500

    added labels

--- quiz/quiz02/exercise01/main.js
+++ quiz/quiz02/exercise01/main.js
@@ -1,21 +1,24 @@
 function showFox() {
     document.querySelector("img").src = "images/fox.jpg";
+    document.querySelector("p").innerHTML = "Fox";
     console.log('Change image and paragraph to fox...');
 }
 
 function showLion() {
     document.querySelector("img").src = "images/lion.jpg";
-
+    document.querySelector("p").innerHTML = "Lion";
     console.log('Change image and paragraph to lion...');
 }
 
 function showTiger() {
     document.querySelector("img").src = "images/tiger.png";
+    document.querySelector("p").innerHTML = "Tiger";
     console.log('Change image and paragraph to tiger...');
 }
 
 function showZebra() {
     document.querySelector("img").src = "images/zebra.jpg";
+    document.querySelector("p").innerHTML = "Zebra";
     console.log('Change image and paragraph to zebra...');
 }
 
