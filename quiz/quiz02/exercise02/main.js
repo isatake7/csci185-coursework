b3a16c69d7d9b93ff896df196d6787cdcb1fbeed
Isa <isasatake7@gmail.com>
2022-11-09 16:06:01 -0500

    quiz02ex02

--- quiz/quiz02/exercise02/main.js
+++ quiz/quiz02/exercise02/main.js
@@ -4,12 +4,41 @@ const canvasHeight = window.innerHeight;
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
 
-    // function invocations goes here:
-
+    drawElmo(100, 100, 150, '#0bc9cd', true);   // nose drawn
+    drawElmo(300, 200, 75, '#0bc9cd', false);   // no nose drawn
+    drawElmo(100, 325, 100, '#8093f1', false);  // no nose drawn
+    drawElmo(250, 375, 125, '#7fb285', true);   // nose drawn
+    drawElmo(550, 200, 250, '#102e4a', true);   // nose drawn
 
 
     drawGrid(canvasWidth, canvasHeight);
 }
 
 
-// function definition goes here:
+function drawElmo(x, y, size, color, hasNose) {
+    //head
+    fill(color);
+    circle(x, y, size);
+
+    //eyes-white
+    fill('white');
+    circle(x-(2*(size/15)), y-(2*(size/10)), size/4);
+    fill('white');
+    circle(x+(2*(size/15)), y-(2*(size/10)), size/4);
+
+    //eyes-pupil
+    fill('black');
+    circle(x-(2*(size/15)), y-(2*(size/10)), size/10);    
+    fill('black');
+    circle(x+(2*(size/15)), y-(2*(size/10)), size/10);
+    
+    //nose
+    if (hasNose == true) {
+        fill('#db5461');
+        ellipse(x, y*1.01, size/4, size/2.90); 
+    }
+
+
+    
+
+}
