cc823c85ff741eba3d429d4a121e3e21ad29175b
Isa <isasatake7@gmail.com>
2022-11-21 12:46:14 -0500

    hw05 done

--- homework/hw05/your_task/main.js
+++ homework/hw05/your_task/main.js
@@ -19,38 +19,46 @@ async function getTracks (term) {
     3. Display the tracks in a nice way to the HTML
     */
 
+    document.querySelector("#tracks").innerHTML = "";
+
     let trackEndpoint = baseURL + "?";
     trackEndpoint += "q=" + term + "&type=track";
 
     const trackData = await fetch(trackEndpoint).then(response => response.json());
     console.log(trackData);
 
-    for (let i = 0; i < 5; i++){
+    if (trackData.length == 0) {
+        document.querySelector("#tracks").innerHTML = "Not found";
+    } else {
+        for (let i = 0; i < 5; i++){
         
-        const trackCard = `
-            <section id="tracks">
-                <section class="track-item preview">
-                    <img src="${trackData[i].album.image_url}" alt = "${trackData[i].name} track image" onclick="playMusic()">
-                    <i class="fas play-track fa-play" aria-hidden="true"></i>
-                    <div class="label">
-                        <h2>${trackData[i].name}</h2>
-                        <p> ${trackData[i].artist.name} </p>
-                    </div>
+            const trackCard = `
+                <section id="tracks">
+                    <section class="track-item preview" onclick="playMusic('${trackData[i].id}')" >
+                        <img src="${trackData[i].album.image_url}" alt = "${trackData[i].name} track image">
+                        <i class="fas play-track fa-play" aria-hidden="true"></i>
+                        <div class="label">
+                            <h2>${trackData[i].name}</h2>
+                            <p> ${trackData[i].artist.name} </p>
+                        </div>
+                    </section>
                 </section>
-            </section>
-        `;
-
-        document.querySelector("#tracks").innerHTML += trackCard;
+            `;
+    
+            document.querySelector("#tracks").innerHTML += trackCard;
+        }
     }
 
-    let i = 0;
-    function playMusic() {
-        i++;
-        document.querySelector("#artist-section h1").innerHTML = "Now Playing";
-   
+
+
+}
+
+function playMusic(trackid) {
+    document.querySelector("#artist-section h1").innerHTML = "Now Playing";
+
     const nowPlaying = `
         <iframe style="border-radius:12px" 
-            src="https://open.spotify.com/embed/track/${trackData[i].id}?utm_source=generator&theme=0" 
+            src="https://open.spotify.com/embed/track/${trackid}?utm_source=generator&theme=0" 
             width="100%" 
             height="152" 
             frameBorder="0" 
@@ -59,84 +67,76 @@ async function getTracks (term) {
             loading="lazy">
         </iframe>
     `;
-    document.querySelector("#artist").innerHTML += nowPlaying;
-}
-
+    document.querySelector("#artist").innerHTML = nowPlaying;
 }
 
 async function getAlbums (term) {
+
+    document.querySelector("#albums").innerHTML = "";
+
+
     let albumEndpoint = baseURL + "?";
     albumEndpoint +=  "q=" + term + "&type=album";
 
     const albumData = await fetch(albumEndpoint).then(response => response.json());
     //console.log(albumData);
 
-    for (i = 0; i < albumData.length; i++) {
-        const albumCard = `
-            <section class="album-card" id="${albumData[i].id}">
-                <div>
-                    <img src="${albumData[i].image_url}" alt = "${albumData[i].name} album cover" >
-                    <h2>${albumData[i].name}</h2>
-                    <div class="footer">
-                        <a href="${albumData[i].spotify_url}" target="_blank">
-                        View on Spotify </a>
+    if (albumData.length == 0) {
+        document.querySelector("#albums").innerHTML = "Not found";
+
+    } else {
+        for (i = 0; i < albumData.length; i++) {
+            const albumCard = `
+                <section class="album-card" id="${albumData[i].id}">
+                    <div>
+                        <img src="${albumData[i].image_url}" alt = "${albumData[i].name} album cover" >
+                        <h2>${albumData[i].name}</h2>
+                        <div class="footer">
+                            <a href="${albumData[i].spotify_url}" target="_blank">
+                            View on Spotify </a>
+                        </div>
                     </div>
-                </div>
-            </section>
-        `;
+                </section>
+            `;
 
-        document.querySelector("#albums").innerHTML += albumCard;
+            document.querySelector("#albums").innerHTML += albumCard;
+        }
     }
+}   
 
-}
 
 async function getArtist (term) {
+
+    document.querySelector("#artist").innerHTML = "";
+
+
     let artistEndpoint = baseURL + "?";
     artistEndpoint += "q=" +term + "&type=artist";
 
     const artistData = await fetch(artistEndpoint).then(response => response.json());
     //console.log(artistData);
 
-    const artistCard = `
-        <section class="artist-card" id="${artistData[0].id}">
-            <div>
-                <img src="${artistData[0].image_url}" alt="image of artist searched">
-                <h2>${artistData[0].name}</h2>
-                <div class="footer">
-                    <a href="${artistData[0].spotify_url}" target="_blank">
-                    View on Spotify </a>
+    if (artistData.length == 0) {
+        document.querySelector("#artist").innerHTML = "Not found";
+    } else {
+        const artistCard = `
+            <section class="artist-card" id="${artistData[0].id}">
+                <div>
+                    <img src="${artistData[0].image_url}" alt="image of artist searched">
+                    <h2>${artistData[0].name}</h2>
+                    <div class="footer">
+                        <a href="${artistData[0].spotify_url}" target="_blank">
+                        View on Spotify </a>
+                    </div>
                 </div>
-            </div>
-        </section>
-    `;
+            </section>
+        `;
 
-    document.querySelector("#artist").innerHTML = artistCard;
+        document.querySelector("#artist").innerHTML = artistCard;
+    }
 
 
 };
-
-// let i = 0;
-// function playMusic () {
-//     i++;
-    
-    
-
-//     document.querySelector("#artist-section h1").innerHTML = "Now Playing";
-   
-//     const nowPlaying = `
-//     <iframe style="border-radius:12px" 
-//         src="https://open.spotify.com/embed/track/${trackData[0].id}?utm_source=generator&theme=0" 
-//         width="100%" 
-//         height="152" 
-//         frameBorder="0" 
-//         allowfullscreen="" 
-//         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-//         loading="lazy">
-//     </iframe>
-// `;
-//     document.querySelector("#artist").innerHTML += nowPlaying;
-// }
-
 
 document.querySelector('#search').onkeyup = function (ev) {
     // Number 13 is the "Enter" key on the keyboard
